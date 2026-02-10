20e7abd801f6b8d9e347eeff2bd9adabb88fbab7
emilyamn <169031446+emilyamn@users.noreply.github.com>
2026-02-10 20:51:08 -0300

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,3 @@
-from .config_loader import load_config, load_all_configs, get_device
+from .config_loader import ConfigLoader
 from .data_utils import custom_collate_fn, move_sample_to_device
 from .time_utils import compute_time_axes
