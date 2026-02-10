646f52162f008a66a3f3d06541cefb8541f63df5
emilyamn <169031446+emilyamn@users.noreply.github.com>
2026-02-10 20:59:46 -0300

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,16 @@
-from .config_loader import ConfigLoader
-from .data_utils import custom_collate_fn, move_sample_to_device
+"""
+Módulo de utilitários para o projeto.
+"""
+
+from .config_loader import (ConfigLoader,load_feature_config,load_split_config)
 from .time_utils import compute_time_axes
+from .data_utils import custom_collate_fn, move_sample_to_device
+
+__all__ = [
+    'ConfigLoader',
+    'load_feature_config',
+    'load_split_config',
+    'compute_time_axes',
+    'custom_collate_fn',
+    'move_sample_to_device',
+]
