aee7d792eb1f82cdc1bfd7048e766a9ed6a86193
emilyamn <169031446+emilyamn@users.noreply.github.com>
2026-02-10 21:15:16 -0300

    edits

--- src/__init__.py
+++ src/__init__.py
@@ -12,7 +12,7 @@ from .model.architecture import Seq2SeqHydro
 from .model.layers import StaticEmbedding
 from .training.trainer import train_model, predict_autoregressive
 from .training.losses import multi_step_loss
-from .utils.config_loader import load_config, load_all_configs, get_device
+from .utils.config_loader import ConfigLoader, load_feature_config, load_split_config
 from .utils.data_utils import custom_collate_fn, move_sample_to_device
 from .utils.time_utils import compute_time_axes
 
@@ -25,7 +25,10 @@ __all__ = [
     "Seq2SeqHydro", "StaticEmbedding",
     # Training
     "train_model", "predict_autoregressive", "multi_step_loss",
-    # Utils
-    "load_config", "load_all_configs", "get_device",
-    "custom_collate_fn", "move_sample_to_device", "compute_time_axes",
+    # Utils - Config
+    "ConfigLoader", "load_feature_config", "load_split_config",
+    # Utils - Data
+    "custom_collate_fn", "move_sample_to_device",
+    # Utils - Time
+    "compute_time_axes",
 ]
